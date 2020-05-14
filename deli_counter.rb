60ce17f3b072e858d58555d0fc5c4ca7771ec509
Sam Rourke <samuelvanrourke@gmail.com>
2020-05-14 00:36:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,13 @@
 def line(array)
   if array.count == 0
     puts "The line is currently empty."
+  else
+    line_list = Array.new
+    line_list << "The line is currently:"
+    array.each_with_index do |name, index|
+      line_list << "#{index + 1}.#{name}"
+    end
+    return line_list.join(" ")
   end
 end
 
