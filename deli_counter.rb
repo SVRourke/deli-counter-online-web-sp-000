875e7f2ef86bc313983ad1bcb7283f896b73bec8
Sam Rourke <samuelvanrourke@gmail.com>
2020-05-14 00:39:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,10 @@
 # Write your code here.
 
 def line(array)
-  puts array.count
   if array.count == 0
     puts "The line is currently empty."
   else
+    puts array.count
     line_list = Array.new
     line_list << "The line is currently:"
     array.each do |name|
